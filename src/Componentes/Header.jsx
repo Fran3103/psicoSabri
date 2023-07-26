cc926f69076704062861f5e8d3844bf75f5809e5
Franco Aguirre <fn.aguirre@hotmail.com>
2023-07-25 22:25:34 -0300

    iniciando carousel

--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -1,45 +1,76 @@
 import React from 'react'
-import style from './header.module.css'
+import './header.css'
 import imagen from '../../src/Imagenes/foto.png'
-
+import imagen2 from '../../src/Imagenes/Tablet/chica-libro.jpg'
+import imagen3 from '../../src/Imagenes/Tablet/chicos-espalda.jpg'
 
 const Header = () => {
   return (
-    <div className={style.contenedor}>
-        <div className={style.imgprincipal}>
-            <img src={imagen} alt="imagen principal" />
-        </div>
-        <div className={style.sobremi}>
-            <h1>
-                Acerca de mí
-            </h1>
-            <h3>
+    
+      <div id="carouselExampleAutoplaying" class="carousel container-md slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <div className="imagenDescripcion">
+                <img src={imagen} class="d-block w-100" alt="..."/>
+                <div className="descripcion">
+                    <h1>
+                        Acerca de mí
+                    </h1>
+                    <h3>
 
-                Sabrina Morocho - Licenciada en Psicología
-            
-            </h3>
-            <h3>
-                Graduada en la Universidad de Ciencias Empresariales y Sociales  (UCES)
-                
-            </h3>
-            <p>
-                Ha realizado estudios de postgrado en Clínica con Adolescentes
-                en la Institución Fernando Ulloa.
+                        Sabrina Morocho - Licenciada en Psicología
+                    
+                    </h3>
+                    <h3>
+                        Graduada en la Universidad de Ciencias Empresariales y Sociales  (UCES)
+                        
+                    </h3>
 
-                La Licenciatura en Psicología me ha permitido adquirir muchos 
-                conocimientos a nivel teórico pero también personal, 
-                ayudándome a ser una profesional que tiene como principio 
-                fundamental la ética, el esfuerzo y la capacitación. 
+                    <p >
+                    Ha realizado estudios de postgrado en Clínica con Adolescentes
+                    en la Institución Fernando Ulloa.
 
-                Al ser una carrera con múltiples aristas, me permite desempeñarme
-                en diversas áreas de interés y a su vez, seguir aprendiendo y 
-                formándome constantemente.
+                    La Licenciatura en Psicología me ha permitido adquirir muchos 
+                    conocimientos a nivel teórico pero también personal, 
+                    ayudándome a ser una profesional que tiene como principio 
+                    fundamental la ética, el esfuerzo y la capacitación. 
 
-            </p>
+                    Al ser una carrera con múltiples aristas, me permite desempeñarme
+                    en diversas áreas de interés y a su vez, seguir aprendiendo y 
+                    formándome constantemente.
+
+                    </p>
+                </div>
+            </div>
         
         </div>
-
+        <div class="carousel-item">
+        <img src={imagen2} class="d-block w-100" alt="..."/>
+        </div>
+        <div class="carousel-item">
+        <img src={imagen3} class="d-block w-100" alt="..."/>
+        </div>
     </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+    //         <div className={style.contenedor}>
+    //     <div className={style.imgprincipal}>
+    //         <img src={imagen} alt="imagen principal" />
+    //     </div>
+    //     <div className={style.sobremi}>
+        
+            
+        
+    //     </div>
+
+    // </div>
   )
 }
 
